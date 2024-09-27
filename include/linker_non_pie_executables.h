6abcd3604c9b068bd66327a7bcfd64008ea9625c
GuaiYiHu <guaiyihu@foxmail.com>
2024-09-27 21:07:16 +0800

    msm8660-common: Update for new pie config

---
+++ include/linker_non_pie_executables.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const char* linker_non_pie_executables[] = {
+    "/system/bin/bridgemgrd",
+    "/system/bin/cnd",
+    "/system/bin/gpsone_daemon",
+    "/system/bin/mm-pp-daemon",
+    "/system/bin/mm-qcamera-daemon",
+    "/system/bin/netmgrd",
+    "/system/bin/port-bridge",
+    "/system/bin/qmiproxy",
+    "/system/bin/qmuxd",
+    "/system/bin/rmt_storage",
+    "/system/bin/thermald",
+    "/system/bin/usbhub",
+    "/system/bin/usbhub_init",
+    "/system/bin/v4l2-qcamera-app",
+};
