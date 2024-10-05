d0c242107ec1a604c620bee7ee464938f0b58313
GuaiYiHu <guaiyihu@foxmail.com>
2024-10-05 15:02:53 +0800

    msm8660-common: Import sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,9 @@ BOARD_USES_LEGACY_MMAP := true
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_SEPOLICY_DIRS += \
+    $(COMMON_PATH)/sepolicy
+
 # Filesystem
 #BOARD_VOLD_MAX_PARTITIONS := 36
 
