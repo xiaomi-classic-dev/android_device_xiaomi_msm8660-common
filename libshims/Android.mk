b85bcc26933ff4562578ffa9bd94ad755c591a66
GuaiYiHu <guaiyihu@foxmail.com>
2026-04-25 02:28:42 +0800

    msm8660-common: Update libshims for N

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -6,12 +6,16 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
      gui/SensorManager.cpp \
+     ui/GraphicBuffer.cpp \
+     ui/GraphicBufferAllocator.cpp \
+     ui/GraphicBufferMapper.cpp \
      MemoryHeapPmem.cpp \
      MemoryBase.c \
-     SharedBuffer.cpp \
      VectorImpl.cpp
 
-LOCAL_C_INCLUDES += external/safe-iop/include
+LOCAL_C_INCLUDES += \
+    external/safe-iop/include \
+    system/core/libutils
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync
 LOCAL_MODULE := libshim_camera
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
